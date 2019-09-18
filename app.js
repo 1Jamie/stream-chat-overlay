d02f1b8a03d1a9ba22986913f26074c243732a72
jamie <charja113@gmail.com>
2019-09-18 10:33:27 -0500

    removing extranious code

--- app.js
+++ app.js
@@ -61,14 +61,6 @@ client.addListener('message', function(from, message) {
 io.sockets.on('connection', function(socket) {
     socket.on('username', function(username) {
         socket.username = username;
-        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
-        console.log(
-            'user ' + socket.username + ' connected'
-        );
-    });
-    socket.on('disconnect', function(username) {
-        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
-    })
 
     socket.on('chat_message', function(message) {
         io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
