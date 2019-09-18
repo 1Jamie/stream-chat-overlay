73541a6f8ee4af8ade44b7493ac8de0c80b06e73
jamie <charja113@gmail.com>
2019-09-18 11:50:24 -0500

    doing some house keeping to remove having the npm modules in the repo

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 var path = require('path');
-var irc = require('irc');
 const tmi = require('tmi.js');
 
 app.use(express.static('public'))
