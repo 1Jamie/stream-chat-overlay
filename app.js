686038915e732f535d9e2fb0cfc8d90e63f7a37c
jamie <charja13@live.com>
2019-09-19 21:41:08 -0500

    type

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const pool = new Pool({
   });
 
 const getCheerUrl = function(cheerW){
-    pool.query(`select cheer url from cheers`).then( res => {
+    pool.query(`select cheer, url from cheers`).then( res => {
         const result = res.rows
         console.log(result)
     })
