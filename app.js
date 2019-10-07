59e5bcf9fdb948010bfe5ffbe49e8bcf8a82269c
jamie <charja113@gmail.com>
2019-10-07 08:58:05 -0500

    this should fix the issues that happened last night with img for chan disappearing

--- app.js
+++ app.js
@@ -192,6 +192,14 @@ function onMessageHandler(channel, tags, message, self) {
         const name = `[${id}] ${display_name} (${login})`;
         const props = `${broadcaster_type}, ${view_count} view(s), image: ${profile_image_url}`;
         // console.log(`${name} -- ${props}`);
+        switch(channel){
+          case '#samma_ftw':
+            channelLogo=`<img align="left" style="padding-right: 3px;" class="chanlogo" src="/css/samma-logo.png" alt="null" id="itemImg">`;
+            break; 
+          case '#charja113':
+            channelLogo=`<img align="left" style="padding-right: 3px;" class="chanlogo" src="/css/charja_logo_head.png" alt="null" id="itemImg">`
+            break;
+        }
         const profileElment = `<img align="left" style="padding-right: 3px;" class="profImg" src="${profile_image_url}" alt="null" id="itemImg">`;
         io.emit('twitch', `${profileElment}<p>${displayName}:</p>  <p>${message1}</p></br>`);
       }
