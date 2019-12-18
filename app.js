28f137f458171615761d6e990b4233ec08d52dd2
Jamie Charlton <charja113@gmail.com>
2019-12-18 18:03:17 -0500

    beautified app.js started building the admin pannel and database adding in the main auth adding display of commands in database currently no actual command usage in chat though has not been migrated to postgresql

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //setting the stuff we need for node (still need to fix deps)
 const express = require('express');
 const app = express();
-const { Pool } = require('pg');
+const {
+  Pool
+} = require('pg');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const tmi = require('tmi.js');
@@ -27,7 +29,7 @@ const headers = {
 //setup our postgresql information we are going to need 
 const pool = new Pool({
   user: 'root',
-  host: 'localhost',
+  host: '192.168.30.102',
   database: 'emotes',
   password: 'password',
   port: 5432,
@@ -37,19 +39,25 @@ const pool = new Pool({
 function helix(endpoint, qs) {
   const queryString = new URLSearchParams(qs);
   const url = `https://api.twitch.tv/helix/${endpoint}?${queryString}`;
-  return fetch(url, { headers })
+  return fetch(url, {
+      headers
+    })
     .then((res) => res.json());
 }
 
 //this does does the obvious... gets users info using helix call
 function getUser(id) {
-  return helix('users', { id })
-    .then(({ data: [user] }) => user || null).catch(console.log("here!"));
+  return helix('users', {
+      id
+    })
+    .then(({
+      data: [user]
+    }) => user || null).catch(console.log("here!"));
 }
 
 //this is going to handle our cheers when called console logs left for debugging if needed later
 const getCheerUrl = function (cheerW, usr) {
-  pool.query('select cheer, url from cheers').then((res) => {
+  pool.query('select cheer, url from cheers order by id desc').then((res) => {
     const result = res.rows;
     let message1;
     let done;
@@ -83,8 +91,12 @@ const getCheerUrl = function (cheerW, usr) {
             // console.log('User not found');
           } else {
             const {
-              id, display_name, login,
-              broadcaster_type, view_count, profile_image_url,
+              id,
+              display_name,
+              login,
+              broadcaster_type,
+              view_count,
+              profile_image_url,
             } = user;
             //const name = `[${id}] ${display_name} (${login})`;
             //const props = `${broadcaster_type}, ${view_count} view(s), image: ${profile_image_url}`;
@@ -107,7 +119,7 @@ const config = {
   },
   channels: [
 
-   'charja113'
+    'charja113'
 
   ],
 };
@@ -117,6 +129,74 @@ function onCheer(channel, userstate, message) {
   getCheerUrl(message, userstate);
 }
 
+//We are gonna deal with the admin pannel here, it will both start up authentication and then were gonna server up the bot commands
+function onAdminCon(conninfo) {
+  console.log(conninfo);
+  const text = 'select * from users where password=$2 and username=$1'
+  pool.query(text, conninfo, (err, res) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      //console.log(res);
+      if (res.rowCount != 0) {
+        io.emit('authEmit', 'permit')
+        //const commands_query = "select * from commands where user_name='" + conninfo[0] +"'";
+        //console.log(commands_query)
+        pool.query('select command_name, response, id from commands where user_name=$1', [conninfo[0]], (err, res) => {
+          if (err) {
+            console.log(err.stack)
+          } else {
+            //console.log(res)
+            io.emit('sendcommands', res)
+          }
+        })
+      } else {
+        io.emit('authEmit', 'deny')
+      }
+    }
+  })
+}
+function onCmdChange(cmdInf){
+  const cmdtxt = 'update commands set response=$2 where id=$1'
+  pool.query(cmdtxt, cmdInf, (err, res) => {
+    if (err) {
+      console.log(err.stack)
+    } else {
+      console.log(`command updated with values ${cmdInf}`)
+    }
+  })
+}
+
+function ondeleteCmd(cmdInf){
+  const text = 'delete from commands where id=$1';
+  pool.query(text, cmdInf, (err, res) => {
+    if (err) {
+    console.log(err.stack)
+  } else {
+    console.log(`command ${cmdInf} deleted`)
+  }
+  })
+}
+function onaddCmd(cmdInf){
+  const text = "insert into commands (user_name, command_name, response) values ($3 , $1 , $2)"
+  pool.query(text, cmdInf, (err, res) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(`command ${cmdInf} added`)
+      let userar = [ cmdInf[2] ]
+      console.log(userar)
+      pool.query('select command_name, response, id from commands where user_name=$1', userar, (err, res) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+          //console.log(res)
+          io.emit('sendcommands', res)
+        }
+      })
+    }
+  })
+}
 //this is going to define the obvious "connectionHadler"
 function onConnectedHandler(addr, port) {
   // console.log(`* Connected to ${addr}:${port}`);
@@ -127,15 +207,59 @@ function onMessageHandler(channel, tags, message, self) {
   let message1;
 
   const {
-    'user-name': username, 'display-name': displayName, 'user-id': userID, subscriber: sub, emotes: emote,
+    'user-name': username,
+    'display-name': displayName,
+    'user-id': userID,
+    subscriber: sub,
+    emotes: emote,
   } = tags;
-  const commandName = message.trim();
 
-  if (commandName === '!project') {
+  const commandName = [ message.trim() , channel.substr(1) ]
+  
+  pool.query('select response from commands where command_name=$1 and user_name=$2', commandName, (err, res) => {
+    console.log( commandName )
+    console.log(res)
+    if (res.rowCount != 0) {
+      client.say(channel, res.rows[0].response)
+    }
+  })
+
+  
+  /*if (commandName === '!project') {
     client.say(channel, 'the project page is https://gitlab.streamchatoverlay.xyz/jamie/streamchatoverlay');
     console.log('project command seen, message should be sent');
+  }*/
+  if (commandName === '!multimsg') {
+    if ((displayName === 'Charja113') || (displayName === 'Samma_FTW')) {
+      if (multi === 'off') {
+        multi = 'on'
+        console.log('multi link turned on')
+        client.join('samma_ftw')
+        //client.say('samma_ftw', 'multi link message: on')
+        client.say('charja113', 'multi link message: on')
+        multiIntervalc = setInterval(function () {
+          client.say('charja113', "Want to get in the action from both sides? The Multi twitch link to watch both streams is http://multitwitch.tv/charja113/samma_ftw")
+        }, 500000);
+        //multiIntervalm =  setInterval(function(){
+        // client.say('samma_ftw', "Want to get in the action from both sides? The Multi twitch link to watch both streams is http://multitwitch.tv/charja113/samma_ftw")
+        //}, 500000);
+        return multi
+      } else {
+        clearInterval(multiIntervalc);
+        //clearInterval(multiIntervalm);
+        client.part('samma_ftw')
+        //client.say('samma_ftw', 'multi link message: off')
+        client.say('charja113', 'multi link message: off')
+        multi = 'off'
+        console.log('multi link turned off')
+        return multi
+      }
+    }
   }
-  
+
+  if (commandName === '!multitwitch') {
+    client.say(channel, "Want to get in the action from both sides? The Multi twitch link to watch both streams is http://multitwitch.tv/charja113/samma_ftw")
+  }
   if ((displayName === 'StreamElements') || (displayName === 'PretzelRocks') || (displayName === 'Charja113') || (displayName === 'Samma_FTW')) {
     console.log('botmessage or streamer');
     return;
@@ -162,9 +286,10 @@ function onMessageHandler(channel, tags, message, self) {
         console.log('emote :/ was seen, bypassing second replace to prevent infinte loop issue # 6');
       } else {
         while (message1.indexOf(rmWord) !== -1) {
-        message1 = message1.replace(rmWord, emoteUrl)
+          message1 = message1.replace(rmWord, emoteUrl)
           console.log('there was more than one instance repalcing')
-        }}
+        }
+      }
       // console.log(message1);
       return message1;
     });
@@ -178,18 +303,22 @@ function onMessageHandler(channel, tags, message, self) {
         // console.log('User not found');
       } else {
         const {
-          id, display_name, login,
-          broadcaster_type, view_count, profile_image_url,
+          id,
+          display_name,
+          login,
+          broadcaster_type,
+          view_count,
+          profile_image_url,
         } = user;
         let channelLogo
         const name = `[${id}] ${display_name} (${login})`;
         const props = `${broadcaster_type}, ${view_count} view(s), image: ${profile_image_url}`;
-        switch(channel){
+        switch (channel) {
           case '#samma_ftw':
-            channelLogo=`<img align="left" style="padding-right: 3px;" class="chanlogo" src="/css/samma-logo.png" alt="null" id="itemImg">`;
-            break; 
+            channelLogo = `<img align="left" style="padding-right: 3px;" class="chanlogo" src="/css/samma-logo.png" alt="null" id="itemImg">`;
+            break;
           case '#charja113':
-            channelLogo=`<img align="left" style="padding-right: 3px;" class="chanlogo" src="/css/charja_logo_head.png" alt="null" id="itemImg">`
+            channelLogo = `<img align="left" style="padding-right: 3px;" class="chanlogo" src="/css/charja_logo_head.png" alt="null" id="itemImg">`
             break;
         }
 
@@ -213,17 +342,17 @@ io.sockets.on('connection', (socket) => {
     socket.username = username;
     // console.log(  `user ${socket.username} connected`,);
   });
-
+  socket.on('adminCon', onAdminCon);
+  socket.on('command_change', onCmdChange);
+  socket.on('newcmd', onaddCmd);
+  socket.on('deleteCmd', ondeleteCmd)
   socket.on('chat_message', (message) => {
     io.emit('chat_message', `<strong>${socket.username}</strong>: ${message}`);
   });
 });
 // this is going to be the handling of the admin page events
-io.sockets.on('adminCon', (info) => {
-console.log(info)
-io.emit('adminEmit', 'it worked')
-})
 
-const server = http.listen(80, () => {
-  console.log('listening on *:80');
+
+const server = http.listen(8083, () => {
+  console.log('listening on *:8083');
 });
